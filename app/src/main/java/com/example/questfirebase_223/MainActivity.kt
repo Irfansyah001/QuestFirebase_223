cc6b7d5823bef6afdf0ed33bbe6e823c0467fbdf
Irfansyah001 <syahirfan545@gmail.com>
2026-01-02 22:35:44 +0700

    feat: Create MainActivity and set up app entry point
    Creates the `MainActivity.kt` file, which serves as the main entry point for the application.
    
    Key configurations include:
    - Extends `ComponentActivity` and uses `setContent` to establish a Jetpack Compose UI.
    - Enables edge-to-edge display for a modern, immersive layout.
    - Integrates the `QuestFirebase_223Theme` to apply consistent styling.
    - Sets `DataSiswaApp` as the main composable, embedding it within a `Scaffold` to provide a basic Material Design layout structure.

---
+++ app/src/main/java/com/example/questfirebase_223/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.questfirebase_223
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.questfirebase_223.ui.theme.QuestFirebase_223Theme
+import com.example.questfirebase_223.view.controllNavigasi.DataSiswaApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestFirebase_223Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DataSiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
